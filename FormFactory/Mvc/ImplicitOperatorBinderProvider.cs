464361260eecea378fb8b54cf1b2daa0e273c1a0
mcintyre321 <mcintyre321@gmail.com>
2011-12-13 17:02:56 +0000

    More trying to get Nuget to work :(

---
+++ FormFactory/Mvc/ImplicitOperatorBinderProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace FormFactory.Mvc
+{
+    public class ImplicitOperatorBinderProvider : IModelBinderProvider
+    {
+        static MethodInfo GetImplicitConversionMethod(Type modelType)
+        {
+            return modelType.GetMethods(BindingFlags.Public | BindingFlags.DeclaredOnly | BindingFlags.Static)
+                  .Where(x => x.Name == "op_Implicit")
+                  .Where(x => modelType.IsAssignableFrom(x.ReturnType))
+              .Where(x => x.GetParameters().Single().ParameterType == typeof(string))
+                  .FirstOrDefault();
+        }
+        #region Implementation of IModelBinderProvider
+
+        public IModelBinder GetBinder(Type modelType)
+        {
+            var implicitConversionMethod = GetImplicitConversionMethod(modelType);
+            if (implicitConversionMethod == null) //there is no implicit operator for binding
+                return null;
+
+            return new ImplicitOperatorBinder(implicitConversionMethod);
+        }
+        class ImplicitOperatorBinder : IModelBinder
+        {
+            private readonly MethodInfo _implicitConversionMethod;
+
+            public ImplicitOperatorBinder(MethodInfo implicitConversionMethod)
+            {
+                _implicitConversionMethod = implicitConversionMethod;
+            }
+
+            public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+            {
+                try
+                {
+                    var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+                    bindingContext.ModelState.SetModelValue(bindingContext.ModelName, value);
+                    var rawValue = (value == null ? null : value.RawValue as string[]) ?? new string[] { };
+                    return _implicitConversionMethod.Invoke(null, new object[] { rawValue.SingleOrDefault() });
+                }
+                catch (TargetInvocationException ex)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, ex.InnerException);
+                    return null;
+                }
+            }
+        }
+        #endregion
+    }
+}
