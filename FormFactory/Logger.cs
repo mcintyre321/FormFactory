be22b2047081ce5601c243d23282c7ad613214c6
mcintyre321 <mcintyre321@gmail.com>
2012-05-07 14:54:03 +0100

    Added logger

---
+++ FormFactory/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FormFactory
+{
+    public class Logger
+    {
+        static Logger()
+        {
+            Trace = s => System.Diagnostics.Trace.WriteLine(s);
+            LogError = s => System.Diagnostics.Trace.TraceError(s.ToString());
+        }
+        public static Action<string> Trace { get; set; }
+
+        public static Action<Exception> LogError { get; set; }
+    }
+}
