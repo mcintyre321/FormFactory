a3e544c3ba3fbfc62a8fad4a4d8edefeaf90280f
Harry Mcintyre <mcintyre321@gmail.com>
2012-05-10 17:41:02 +0100

    Updated example to include some goodness

---
+++ packages/EmbeddedResourceVirtualPathProvider.1.2.5/content/App_Code/RegisterVirtualPathProvider.cs.pp
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Linq;
+namespace $rootnamespace$.App_Code
+{
+    public class RegisterVirtualPathProvider
+    {
+        public static void AppInitialize()
+        {
+			//By default, we scan all non system assemblies for embedded resources
+            var assemblies = System.Web.Compilation.BuildManager.GetReferencedAssemblies()
+                .Cast<Assembly>()
+                .Where(a => a.GetName().Name.StartsWith("System") == false);
+            System.Web.Hosting.HostingEnvironment.RegisterVirtualPathProvider(new EmbeddedResourceVirtualPathProvider.Vpp(assemblies.ToArray())
+            {
+				//you can do a specific assembly registration too. If you provide the assemly source path, it can read
+				//from the source file so you can change the content while the app is running without needing to rebuild
+				//{typeof(SomeAssembly.SomeClass).Assembly, @"..\SomeAssembly"} 
+            });
+        }
+    }
+}
