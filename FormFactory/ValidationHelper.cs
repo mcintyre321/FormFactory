db76ea6ee28614a0f22eea90bae7f7ce387857e3
Harry Mcintyre <mcintyre321@gmail.com>
2012-06-20 17:03:12 +0100

    Made attribute rules pluggable

--- FormFactory/ValidationHelper.cs
+++ FormFactory/ValidationHelper.cs
@@ -28,26 +28,67 @@ namespace FormFactory
                    helper.ViewData.ModelState[modelName].Errors.Count > 0;
         }
 
+        public static List<UnobtrusiveValidationRule> UnobtrusiveValidationRules = new List<UnobtrusiveValidationRule>()
+        {
+            GetRulesFromAttributes
+        };
+
+
+
+        private static IEnumerable<ModelClientValidationRule> GetRulesFromAttributes(PropertyVm property)
+        {
+            return property.GetCustomAttributes()
+                .SelectMany(attribute => UnobtrusiveValidationAttributeRules,
+                            (attribute, rule) => rule(property, attribute))
+                .Where(r => r != null);
+        }
+
+        public static List<UnobtrusiveValidationAttributeRule> UnobtrusiveValidationAttributeRules = new List
+          <UnobtrusiveValidationAttributeRule>()
+        {
+            RangeAttribteRule,
+            RequiredAttributeRule,
+            StringLengthAttributeRule,
+            RegexAttributeRule
+        };
+
+        private static ModelClientValidationRule RangeAttribteRule(PropertyVm propertyVm, object attribute)
+        {
+            var a = attribute as RangeAttribute;
+            return (a == null) ? null : new ModelClientValidationRangeRule(a.FormatErrorMessage(propertyVm.DisplayName), a.Minimum, a.Maximum);
+        }
+
+        private static ModelClientValidationRule RequiredAttributeRule(PropertyVm propertyVm, object attribute)
+        {
+            var a = attribute as RequiredAttribute;
+            return (a == null) ? null : new ModelClientValidationRequiredRule(a.FormatErrorMessage(propertyVm.DisplayName));
+        }
+
+        private static ModelClientValidationRule StringLengthAttributeRule(PropertyVm propertyVm, object attribute)
+        {
+            var a = attribute as StringLengthAttribute;
+            return (a == null) ? null : new ModelClientValidationStringLengthRule(a.FormatErrorMessage(propertyVm.DisplayName), a.MinimumLength, a.MaximumLength);
+        }
+        private static ModelClientValidationRule RegexAttributeRule(PropertyVm propertyVm, object attribute)
+        {
+            var a = attribute as RegularExpressionAttribute;
+            return (a == null) ? null : new ModelClientValidationRegexRule(a.FormatErrorMessage(propertyVm.DisplayName), a.Pattern);
+        }
+
+        public delegate IEnumerable<ModelClientValidationRule> UnobtrusiveValidationRule(PropertyVm property);
+        public delegate ModelClientValidationRule UnobtrusiveValidationAttributeRule(PropertyVm property, object attribute);
+
+
+
+
         public static MvcHtmlString UnobtrustiveValidation(this PropertyVm property)
         {
             var sb = new StringBuilder();
 
-            var rules = new List<ModelClientValidationRule>();
-
-            var attributes = property.GetCustomAttributes();
-            attributes.OfType<RangeAttribute>()
-                .Each(a => rules.Add(new ModelClientValidationRangeRule(a.FormatErrorMessage(property.DisplayName), a.Minimum, a.Maximum))).Eval();
-
-            attributes.OfType<RequiredAttribute>()
-                .Each(a => rules.Add(new ModelClientValidationRequiredRule(a.FormatErrorMessage(property.DisplayName)))).Eval();
-
-            attributes.OfType<StringLengthAttribute>()
-                .Each(a => rules.Add(new ModelClientValidationStringLengthRule(a.FormatErrorMessage(property.DisplayName), a.MinimumLength, a.MaximumLength))).Eval();
-
-            attributes.OfType<RegularExpressionAttribute>()
-                            .Each(a => rules.Add(new ModelClientValidationRegexRule(a.FormatErrorMessage(property.DisplayName), a.Pattern))).Eval();
+            var rules = UnobtrusiveValidationRules.SelectMany(r => r(property));
 
             if (rules.Any() == false) return new MvcHtmlString("");
+
             sb.Append("data-val=\"true\" ");
             foreach (var rule in rules)
             {
