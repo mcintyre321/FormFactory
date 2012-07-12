f2779272bbf0d8a958e59b06944a956088fac2f5
Tim Iles <tim@timiles.com>
2012-07-12 16:17:12 +0100

    slightly neater file uploader

--- FormFactory/Mvc/AppDataFileUploader.cs
+++ FormFactory/Mvc/AppDataFileUploader.cs
@@ -8,13 +8,27 @@ namespace FormFactory.Mvc
     {
         public static string Upload(HttpPostedFileBase file)
         {
-            var filepath = Guid.NewGuid().ToString().Replace("-", "") +
-                           "\\" + Path.GetFileName(file.FileName);
-            var storePath = Path.Combine(AppDomain.CurrentDomain.GetData("DataDirectory").ToString(),
-                                         "UploadedFiles\\", filepath);
-            Directory.CreateDirectory(Path.GetDirectoryName(storePath));
-            file.SaveAs(storePath);
-            return filepath;
+            var folder = Path.Combine(AppDomain.CurrentDomain.GetData("DataDirectory").ToString(),
+                                      "UploadedFiles");
+            Directory.CreateDirectory(folder);
+
+            int offset = 0;
+            string filePath;
+            do
+            {
+                filePath = folder + "\\" + GetOffsetFileName(file.FileName, offset);
+                offset++;
+            } while (File.Exists(filePath));
+            
+            file.SaveAs(filePath);
+            return filePath;
+        }
+        static string GetOffsetFileName(string fileName, int offset)
+        {
+            if (offset == 0) return fileName;
+            var parts = fileName.Split('.');
+            parts[Math.Max(parts.Length - 2, 0)] += " (" + offset + ")";
+            return string.Join(".", parts);
         }
     }
 }
