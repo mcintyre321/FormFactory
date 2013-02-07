4e6602c9dd498087720017d1b055fa34c6f59513
Harry McIntyre <mcintyre321@gmail.com>
2013-02-07 21:48:31 +0000

    Added hook to control which properties get rendered from an object

--- FormFactory/FormHelperExtension.cs
+++ FormFactory/FormHelperExtension.cs
@@ -151,15 +151,28 @@ namespace FormFactory
 
     public static class ModelHelper
     {
+        public static Func<HtmlHelper, object, Type, IEnumerable<PropertyVm>> GetPropertyVms { get; set; }
+        
+        static ModelHelper()
+        {
+            GetPropertyVms = GetPropertyVmsUsingReflection;
+        }
+        
         public static IEnumerable<PropertyVm> PropertiesFor<T>(this HtmlHelper helper, T model)
         {
             return helper.PropertiesFor(model, typeof(T));
         }
+       
         public static IEnumerable<PropertyVm> PropertiesFor(this HtmlHelper helper, object model, Type fallbackModelType)
+        {
+            return GetPropertyVms(helper, model, fallbackModelType);
+        }
+
+        private static IEnumerable<PropertyVm> GetPropertyVmsUsingReflection(HtmlHelper helper, object model, Type fallbackModelType)
         {
             var type = model != null ? model.GetType() : fallbackModelType;
 
-            var typeVm = new PropertyVm(helper, typeof(string), "__type");
+            var typeVm = new PropertyVm(helper, typeof (string), "__type");
             typeVm.IsHidden = true;
 
             typeVm.Value = PolymorphicModelBinder.WriteTypeToString(type);
@@ -177,17 +190,18 @@ namespace FormFactory
                 PropertyInfo choices = properties.SingleOrDefault(p => p.Name == property.Name + "_choices");
                 if (choices != null)
                 {
-                    inputVm.Choices = (IEnumerable)choices.GetGetMethod().Invoke(model, null);
-                } 
+                    inputVm.Choices = (IEnumerable) choices.GetGetMethod().Invoke(model, null);
+                }
                 PropertyInfo suggestions = properties.SingleOrDefault(p => p.Name == property.Name + "_suggestions");
                 if (suggestions != null)
                 {
-                    inputVm.Suggestions = (IEnumerable)suggestions.GetGetMethod().Invoke(model, null);
+                    inputVm.Suggestions = (IEnumerable) suggestions.GetGetMethod().Invoke(model, null);
                 }
 
                 yield return inputVm;
             }
         }
+
         public static void Render(this IEnumerable<PropertyVm> properties)
         {
             foreach (var propertyVm in properties)
