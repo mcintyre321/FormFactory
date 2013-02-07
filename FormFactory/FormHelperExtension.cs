6530642f1fbabdf0140370b3d982ca196a8c9ea9
Harry McIntyre <mcintyre321@gmail.com>
2013-02-07 21:55:21 +0000

    Extracted into VmHelper.cs

--- FormFactory/FormHelperExtension.cs
+++ FormFactory/FormHelperExtension.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
-using FormFactory.Mvc;
 
 namespace FormFactory
 {
@@ -147,78 +144,5 @@ namespace FormFactory
         }
 
         
-    }
-
-    public static class ModelHelper
-    {
-        public static Func<HtmlHelper, object, Type, IEnumerable<PropertyVm>> GetPropertyVms { get; set; }
-        
-        static ModelHelper()
-        {
-            GetPropertyVms = GetPropertyVmsUsingReflection;
-        }
-        
-        public static IEnumerable<PropertyVm> PropertiesFor<T>(this HtmlHelper helper, T model)
-        {
-            return helper.PropertiesFor(model, typeof(T));
-        }
-       
-        public static IEnumerable<PropertyVm> PropertiesFor(this HtmlHelper helper, object model, Type fallbackModelType)
-        {
-            return GetPropertyVms(helper, model, fallbackModelType);
-        }
-
-        private static IEnumerable<PropertyVm> GetPropertyVmsUsingReflection(HtmlHelper helper, object model, Type fallbackModelType)
-        {
-            var type = model != null ? model.GetType() : fallbackModelType;
-
-            var typeVm = new PropertyVm(helper, typeof (string), "__type");
-            typeVm.IsHidden = true;
-
-            typeVm.Value = PolymorphicModelBinder.WriteTypeToString(type);
-            yield return typeVm;
-            var properties = type.GetProperties();
-
-            foreach (var property in properties)
-            {
-                if (properties.Any(p => p.Name + "Choices" == property.Name))
-                {
-                    continue; //skip this is it is choice
-                }
-
-                var inputVm = new PropertyVm(model, property, helper);
-                PropertyInfo choices = properties.SingleOrDefault(p => p.Name == property.Name + "_choices");
-                if (choices != null)
-                {
-                    inputVm.Choices = (IEnumerable) choices.GetGetMethod().Invoke(model, null);
-                }
-                PropertyInfo suggestions = properties.SingleOrDefault(p => p.Name == property.Name + "_suggestions");
-                if (suggestions != null)
-                {
-                    inputVm.Suggestions = (IEnumerable) suggestions.GetGetMethod().Invoke(model, null);
-                }
-
-                yield return inputVm;
-            }
-        }
-
-        public static void Render(this IEnumerable<PropertyVm> properties)
-        {
-            foreach (var propertyVm in properties)
-            {
-                propertyVm.Html.RenderPartial("FormFactory/Form.Property", propertyVm);
-            }
-        }
-        public static MvcHtmlString ToMvcHtmlString(this IEnumerable<PropertyVm> properties)
-        {
-            var sb = new StringBuilder();
-            foreach (var propertyVm in properties)
-            {
-                sb.AppendLine(propertyVm.Html.Partial("FormFactory/Form.Property", propertyVm).ToHtmlString());
-            }
-            var mvcHtmlString = new MvcHtmlString(sb.ToString());
-            return mvcHtmlString;
-
-        }
     }
 }
