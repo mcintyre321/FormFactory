c7b0291b5ba4126c7eb60a72b0b1434ef1578c7b
mcintyre321 <mcintyre321@gmail.com>
2013-06-03 13:33:38 +0100

    Working without ASP.NET

---
+++ FormFactory.Tests/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FormFactory.RazorEngine;
+using NUnit.Framework;
+
+namespace FormFactory.Tests
+{
+    public class BasicPropertyTests
+    {
+        [Test]
+        public void CanRenderAPropertyWithoutThrowingAnException()
+        {
+            var someObject = new SomeType() {SomeProperty = "SomeValue"};
+            var properties = Properties.For(someObject);
+            var annotation = new System.ComponentModel.DataAnnotations.DisplayAttribute();
+
+            var html = properties.Render().ToString().Trim();
+            Assert.That(string.IsNullOrWhiteSpace(html) == false);
+        }
+
+        
+    }
+    public class SomeType
+    {
+        public string SomeProperty { get; set; }
+    }
+}
