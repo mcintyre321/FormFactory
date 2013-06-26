e2eb086535ed25867c339eae41f2f261704d1613
Harry McIntyre <mcintyre321@gmail.com>
2013-06-26 14:39:13 +0100

    Updated AssemblyInfos

--- FormFactory.AspMvc/Properties/AssemblyInfo.cs
+++ FormFactory.AspMvc/Properties/AssemblyInfo.cs
@@ -6,14 +6,16 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FormFactory.AspMvc")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Advanced object to html form transformer. FormFactory.AspMvc should be used with ASP MVC. See http://formfactory.apphb.com/")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FormFactory.AspMvc")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Harry McIntyre Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -32,5 +34,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+ 
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- FormFactory.RazorEngine/Properties/AssemblyInfo.cs
+++ FormFactory.RazorEngine/Properties/AssemblyInfo.cs
@@ -6,14 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FormFactory.RazorEngine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Object to html form transformer. FormFactory.RazorEngine can be embedded in WebApi, Nancy and Console apps. See http://formfactory.apphb.com/")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FormFactory.RazorEngine")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Harry McIntyre Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
