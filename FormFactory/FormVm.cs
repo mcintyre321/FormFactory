7113dabc5a268dcac22fe3295d6abde58ac8afd4
mcintyre321 <mcintyre321@gmail.com>
2016-08-14 14:49:20 +0100

    Enable custom enctype on forms

--- FormFactory/FormVm.cs
+++ FormFactory/FormVm.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -6,53 +5,6 @@ using FormFactory.Attributes;
 
 namespace FormFactory
 {
-    public class FormRenderScope : IDisposable {
-        public FfHtmlHelper HtmlHelper { get; set; }
-        public FormVm FormVm { get; private set; }
-
-        public FormRenderScope(FfHtmlHelper htmlHelper, FormVm formVm)
-        {
-            HtmlHelper = htmlHelper;
-            FormVm = formVm;
-        }
-
-        public FormRenderScope Render()
-        {
-            RenderStart();
-            RenderActionInputs();
-            RenderButtons();
-            return this;
-        }
-
-        public FormRenderScope RenderButtons()
-        {
-            HtmlHelper.RenderPartial("FormFactory/Form.Actions", this.FormVm);
-            return this;
-        }
-
-        public FormRenderScope RenderActionInputs()
-        {
-            foreach (var input in FormVm.Inputs)
-            {
-                HtmlHelper.RenderPartial("FormFactory/Form.Property", input);
-            }
-            return this;
-        }
-
-        public FormRenderScope RenderStart()
-        {
-            HtmlHelper.RenderPartial("FormFactory/Form.Start", this.FormVm);
-            return this;
-        }
-
-
-        public void Dispose()
-        {
-            HtmlHelper.RenderPartial("FormFactory/Form.Close", this.FormVm);
-        }
-   
-    }
-
     public class FormVm : IHasDisplayName
     {
 
@@ -96,10 +48,9 @@ namespace FormFactory
             RenderAntiForgeryToken = Defaults.RenderAntiForgeryToken;
         }
 
-        private string _actionUrlOverride;
         public string ActionUrl { get; set; }
         public string Method { get; set; }
-        
+        public string EncType { get; set; } = "multipart/form-data";
         public IEnumerable<PropertyVm> Inputs { get; set; }
 
 
