69ac6939e51258e2e449e3578ec644bcf13e1090
mcintyre321 <mcintyre321@gmail.com>
2016-08-25 21:58:21 +0100

    More WIP

---
+++ FormFactory.AspNet.Example/Models/Examples/ChoicesExample.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace FormFactory.AspMvc.Example.Models.Examples
+{
+    public class ChoicesExample
+    {
+        [Description("By creating a method called {property}_choices you can create select lists")]
+        public string Gender { get; set; }
+        public IEnumerable<string> Gender_choices()
+        {
+            return "male,female,not specified".Split(',');
+        }
+    }
+}
