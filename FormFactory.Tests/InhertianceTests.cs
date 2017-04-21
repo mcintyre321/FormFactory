016f2c31f360474c11c6c617894f39a2b03574cf
Jens Jonsson <jens.jonsson@wisly.se>
2017-04-21 16:25:23 +0200

    Added support for inheritance of ViewModels.

---
+++ FormFactory.Tests/InhertianceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FormFactory.Tests
+{
+    public class InhertianceTests
+    {
+        [Test]
+        public void FindsInhertitedProperties()
+        {
+            var model = new InhertitingClass();
+            var propertyVmsUsingReflection = FF.GetPropertyVmsUsingReflection(model, typeof(object)).ToList();
+            Assert.IsNotNull(propertyVmsUsingReflection.SingleOrDefault(_ => _.Name == nameof(BaseClass.BaseClassStringProperty)), $"Inherited property '{nameof(BaseClass.BaseClassStringProperty)}' missing.");
+        }
+
+        class BaseClass
+        {
+            public string BaseClassStringProperty { get; set; }
+        }
+
+        class InhertitingClass : BaseClass
+        {
+            public string InheritingClassStringProperty { get; set; }
+        }
+    }
+}
