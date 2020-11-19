a15ccc5b11cb7f29e3aa33bc709b2b1c55380e75
Harry McIntyre <harry.mcintyre@stepstone.com>
2020-11-19 13:14:18 +0000

    better errros

--- FormFactory.AspNetCore.Example/Startup.cs
+++ FormFactory.AspNetCore.Example/Startup.cs
@@ -48,16 +48,7 @@ namespace FormFactory.AspNetCore.Example
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
             
